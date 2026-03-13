a063613e6ac502efb1be3f1965652c9ffdf8be60
AashishPatadiya <155621340+AashishPatadiya@users.noreply.github.com>
2026-03-13 11:25:32 +0530

    Add files via upload

---
+++ p17.py
@@ -0,0 +1,11 @@
+#module with 4 functions
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Code is working!")
+    print(add(5, 5))
